91860629f6732f37a3fd8901df502cf66070a498
monado <monado@xenoblade.cn>
2018-08-02 13:53:10 +0800

    test

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/travis/build/luminisward/Xenoblade2Voice-Vue
+cd 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t luminisward/xb2voice .
+docker build -t luminisward/xb2voice /home/travis/build/luminisward/Xenoblade2Voice-Vue
 docker push luminisward/xb2voice
