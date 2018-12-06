00ef3cdda56a9a4b99abd63159b86a0fe03867b0
monado <monado@xenoblade.cn>
2018-12-06 14:40:05 +0800

    lint

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import iView from 'iview'
 import App from './App.vue'
 import store from './store'
-import 'iview/dist/styles/iview.css';
-
+import 'iview/dist/styles/iview.css'
 
 Vue.use(iView)
 
