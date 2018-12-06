655a7d2902b403d05340099fe570efd2bf996255
monado <monado@xenoblade.cn>
2018-12-06 14:14:59 +0800

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,15 @@ module.exports = {
       'before': true,
       'after': true
     }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
     'block-spacing': [2, 'always'],
     'brace-style': [2, '1tbs', {
       'allowSingleLine': true
